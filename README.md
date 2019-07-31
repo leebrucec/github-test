2d6aebe680f82091981dd2eb462f1537199df9a0
Bruce Lee <leebrucec@gmail.com>
2019-07-30 19:00:26 -0600

    Working version of the assignment - needs some work on the logging. Also, I could not get the user info from the client - this will need to be fixed.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # github-test
+
+DESCRUOTION
+github-test is a simple program written using go and utilizing the go-getgithub library
+API set. The program will add a license to all repositories a github organization that
+the user has permission to do so.
+
+INSTRUCTIONS
+Here are the assumptions that are made for running the program successfully.
+- Go must be set up properly when building the executable (the go-gitbub libraries must be
+installed in the proper directory)
+- The LICENSE (must be named LICENSE) file that will be added to the repositories must exist in the same directory
+as the executable github-test
+- You will be prompted to enter a valid user name and password to authenticate to github
+- You will be prompted to enter an email address
+- You will prompted to enter a valid organization that the user has membership in
+
+If the above steps are correctly followed, then the LICENSE file will be added to a branch
+and a push request will be created for the repository.
+
+IMPROVEMENTS
+- Changes need to be made to fetch the username and email address from the user programmatically
+- Select from a list of licenses and fetch from proper location
+- Better log messages
--- github-test.go
+++ github-test.go
@@ -3,51 +3,37 @@ package main
 import (
 	"bufio"
 	"context"
-	"fmt"
-	"flag"
 	"errors"
+	"flag"
+	"fmt"
+	"github.com/google/go-github/github"
+	"golang.org/x/crypto/ssh/terminal"
 	"io/ioutil"
-	"time"
 	"log"
-
 	"os"
 	"strings"
 	"syscall"
-
-	"github.com/google/go-github/github"
-	"golang.org/x/crypto/ssh/terminal"
+	"time"
 )
 
-var (
-	// owner
-	sourceOwner   = flag.String("source-owner", "leetestorg", "Name of the owner (user or org) of the repo to create the commit in.")
-	// repo
-	sourceRepo    = flag.String("source-repo", "Test1", "Name of repo to create the commit in.")
-	// Message
-	commitMessage = flag.String("commit-message", "Add License to project", "Content of the commit message.")
-	// branch name
-	commitBranch  = flag.String("commit-branch", "license", "Name of branch to create the commit in. If it does not already exists, it will be created using the `base-branch` parameter")
-	baseBranch    = flag.String("base-branch", "master", "Name of branch to create the `commit-branch` from.")
-	// owner
-	prRepoOwner   = flag.String("merge-repo-owner", "leetestorg", "Name of the owner (user or org) of the repo to create the PR against. If not specified, the value of the `-source-owner` flag will be used.")
-	// pull request repo
-	prRepo        = flag.String("merge-repo", "Test1", "Name of repo to create the PR against. If not specified, the value of the `-source-repo` flag will be used.")
-	prBranch      = flag.String("merge-branch", "master", "Name of branch to create the PR against (the one you want to merge your branch in via the PR).")
-	// title
-	prSubject     = flag.String("pr-title", "Pull request title", "Title of the pull request. If not specified, no pull request will be created.")
-	// description
-	prDescription = flag.String("pr-text", "Description of pull request", "Text to put in the description of the pull request.")
-	// source file
-	sourceFiles   = flag.String("files", "LICENSE", `Comma-separated list of files to commit and their location.
+type username string
+
+var sourceOwner = flag.String("source-owner", "", "Name of the owner (user or org) of the repo to create the commit in.")
+var sourceRepo = flag.String("source-repo", "", "Name of repo to create the commit in.")
+var prRepoOwner = flag.String("merge-repo-owner", "", "Name of the owner (user or org) of the repo to create the PR against. If not specified, the value of the `-source-owner` flag will be used.")
+var prRepo = flag.String("merge-repo", "Test1", "Name of repo to create the PR against. If not specified, the value of the `-source-repo` flag will be used.")
+var commitMessage = flag.String("commit-message", "Add License to project", "Content of the commit message.")
+var commitBranch = flag.String("commit-branch", "license", "Name of branch to create the commit in. If it does not already exists, it will be created using the `base-branch` parameter")
+var baseBranch = flag.String("base-branch", "master", "Name of branch to create the `commit-branch` from.")
+var prBranch = flag.String("merge-branch", "master", "Name of branch to create the PR against (the one you want to merge your branch in via the PR).")
+var prSubject = flag.String("pr-title", "Add license file", "Title of the pull request. If not specified, no pull request will be created.")
+var prDescription = flag.String("pr-text", "Add license file to project", "Text to put in the description of the pull request.")
+var sourceFiles = flag.String("files", "LICENSE", `Comma-separated list of files to commit and their location.
 The local file is separated by its target location by a semi-colon.
 If the file should be in the same location with the same name, you can just put the file name and omit the repetition.
 Example: README.md,main.go:github/examples/commitpr/main.go`)
-	// author name
-	authorName  = flag.String("author-name", "leebrucec", "Name of the author of the commit.")
-	// author email
-	authorEmail = flag.String("author-email", "leebrucec@gmail.com", "Email of the author of the commit.")
-)
-
+var authorName = flag.String("author-name", "", "Name of the author of the commit.")
+var authorEmail = flag.String("author-email", "", "Email of the author of the commit.")
 
 var ctx = context.Background()
 
@@ -177,17 +163,17 @@ func createPR(client *github.Client) (err error) {
 //*/
 
 func FetchOrganizationRepositories(client *github.Client) ([]*github.Repository, error) {
-//	client := github.NewClient(nil)
+	//	client := github.NewClient(nil)
 	repos, _, err := client.Repositories.ListByOrg(context.Background(), "leetestorg", nil)
 	return repos, err
 }
 
-func BasicAuthentication() ( *github.Client, string) {
+func BasicAuthentication() (*github.Client, string) {
 	r := bufio.NewReader(os.Stdin)
-	fmt.Print("GitHub Username: ")
+	fmt.Print("Enter GitHub Username: ")
 	username, _ := r.ReadString('\n')
 
-	fmt.Print("GitHub Password: ")
+	fmt.Print("Enter GitHub Password: ")
 	bytePassword, _ := terminal.ReadPassword(int(syscall.Stdin))
 	password := string(bytePassword)
 
@@ -195,34 +181,90 @@ func BasicAuthentication() ( *github.Client, string) {
 		Username: strings.TrimSpace(username),
 		Password: strings.TrimSpace(password),
 	}
-
 	client := github.NewClient(tp.Client())
 
-	return client, username
+	/*
+		user, _, err := client.Users.Get(context.Background(), "")
+		if err != nil {
+			fmt.Printf("Users.Get returned error: %v", err)
+
+		} else {
+			fmt.Printf("\nuser = %v\n", user.Name)
+			fmt.Printf("email = %v\n", user.Email)
+		}
+	*/
+
+	return client, tp.Username
 }
 
-/*
-func FetchRepositoryLicense(repo Repository) (License, error) {
-	repos, _, err := repo.RepositoriesService.License
-	repos, _, err := client.Repositories.ListByOrg(context.Background(), "leetestorg", nil)
-	return repos, err
-}
-*/
+func CreateLicensePullRequest(client *github.Client, repo *github.Repository, orgName string) {
+	sourceOwner = &orgName
+	sourceRepo = repo.Name
+	prRepoOwner = &orgName
+	prRepo = repo.Name
 
-func main() {
+	fmt.Printf("sourceOwner = %v\n", *sourceOwner)
+	fmt.Printf("sourceRepo = %v\n", *sourceRepo)
+	fmt.Printf("sourceFiles = %v\n", *sourceFiles)
+	fmt.Printf("commitBranch = %v\n", *commitBranch)
+	fmt.Printf("authorName = %v\n", *authorName)
+	fmt.Printf("authorEmail = %v\n", *authorEmail)
+
+	// make sure we have the necessary values for the pull request
 	if *sourceOwner == "" || *sourceRepo == "" || *commitBranch == "" || *sourceFiles == "" || *authorName == "" || *authorEmail == "" {
 		log.Fatal("You need to specify a non-empty value for the flags `-source-owner`, `-source-repo`, `-commit-branch`, `-files`, `-author-name` and `-author-email`")
 	}
 
+	// create the commit reference
+	ref, err := getRef(client)
+	if err != nil {
+		log.Fatalf("Unable to get/create the commit reference: %s\n", err)
+	}
 
-	client, username := BasicAuthentication();
+	if ref == nil {
+		log.Fatalf("No error where returned but the reference is nil")
+	}
+
+	// create the tree
+	tree, err := getTree(client, ref)
+	if err != nil {
+		log.Fatalf("Unable to create the tree based on the provided files: %s\n", err)
+	}
+
+	// create the commit
+	if err := pushCommit(client, ref, tree); err != nil {
+		log.Fatalf("Unable to create the commit: %s\n", err)
+	}
+
+	// create the pull request
+	if err := createPR(client); err != nil {
+		log.Fatalf("Error while creating the pull request: %s", err)
+	}
+
+	return
+}
+
+func main() {
+	client, username := BasicAuthentication()
+	if client == nil {
+		return
+	}
+
+	fmt.Printf("\nusername = %v\n", username)
+	authorName = &username
+
+	var eMail string
+	fmt.Print("Enter Github user email: ")
+	fmt.Scanf("%s", &eMail)
+	authorEmail = &eMail
+
+	var orgName string
+	fmt.Print("Enter GitHub organization name: ")
+	fmt.Scanf("%s", &orgName)
 
 	fmt.Printf("username = %v\n", username)
-
-//	if err != nil {
-//		fmt.Printf("\nerror: %v\n", err)
-//		return
-//	}
+	fmt.Printf("email = %v\n", eMail)
+	fmt.Printf("organization = %v\n", orgName)
 
 	repos, err := FetchOrganizationRepositories(client)
 	if err != nil {
@@ -236,75 +278,9 @@ func main() {
 				fmt.Printf("\tName = %v\n", license.GetName())
 				fmt.Printf("\tURL =  %v\n", license.GetURL())
 				fmt.Printf("\tKey =  %v\n", license.GetKey())
+			} else {
+				CreateLicensePullRequest(client, repo, orgName)
 			}
 		}
 	}
-
-
-
-	ref, err := getRef(client)
-	if err != nil {
-		log.Fatalf("Unable to get/create the commit reference: %s\n", err)
-	}
-	if ref == nil {
-		log.Fatalf("No error where returned but the reference is nil")
-	}
-
-	tree, err := getTree(client, ref)
-	if err != nil {
-		log.Fatalf("Unable to create the tree based on the provided files: %s\n", err)
-	}
-
-	if err := pushCommit(client, ref, tree); err != nil {
-		log.Fatalf("Unable to create the commit: %s\n", err)
-	}
-
-	if err := createPR(client); err != nil {
-		log.Fatalf("Error while creating the pull request: %s", err)
-	}
-
-
-
-	// fmt.Printf("\n%v\n", github.Stringify(user))
-
-	//username := user.GetName()
-
-/*
-	fmt.Printf("------- Organizations: %v\n", username)
-	organizations, err := FetchOrganizations(client, username)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
-	} else {
-		for i, organization := range organizations {
-			fmt.Printf("%v. %v\n", i+1, organization.GetLogin())
-		}
-	}
-	fmt.Printf("------ Members: %v\n", username)
-	members, err := FetchOrganizationMembers(username)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
-	} else {
-		for i, member := range members {
-			fmt.Printf("%v. %v\n", i+1, member.GetLogin())
-		}
-	}
-	fmt.Printf("------ repositories: %v\n", username)
-	repos, err := FetchOrganizationRepositories(username)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
-	} else {
-		for i, repo := range repos {
-			fmt.Printf("%v. %v\n", i+1, repo.GetName())
-			license := repo.GetLicense()
-			if license != nil {
-				fmt.Printf("\tName = %v\n", license.GetName())
-				fmt.Printf("\tURL =  %v\n", license.GetURL())
-				fmt.Printf("\tKey =  %v\n", license.GetKey())
-			}
-		}
-	}
-	*/
 }
